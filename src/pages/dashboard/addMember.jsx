0ce38b4c5d5f8a27f7148adb5655db4cf15f6afe
Gareth Edwards <GARETHE@pepstores.com>
2026-02-05 22:00:04 +0200

    Adding church id from user to every member added

--- src/pages/dashboard/addMember.jsx
+++ src/pages/dashboard/addMember.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Card from '../../components/ui/Card';
 import { addMember } from '../../services/api';
+import { useAuth } from '../../contexts/AuthContext';
 
 const AddMember = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
+  const { user } = useAuth();
 
   const [formData, setFormData] = useState({
     firstName: '',
@@ -15,7 +17,8 @@ const AddMember = () => {
     role: 'Member', // Default
     email: '',
     cell: '',
-    dob: ''
+    dob: '',
+    church_id: user.church_id
   });
 
   const handleChange = (e) => {
